104bb21260aa943d70ee583c2050e6125b11d70b
MathieuMariotDev <60600228+MathieuMariotDev@users.noreply.github.com>
2021-06-03 10:51:14 +0200

    Add code

---
+++ P4_05_Code_MARIOT_Mathieu/app/src/main/java/com/example/mareu/Views/MeetingViewHolder.java
@@ -0,0 +1,47 @@
+package com.example.mareu.Views;
+
+import android.text.Html;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.mareu.R;
+import com.example.mareu.modele.Meeting;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MeetingViewHolder extends RecyclerView.ViewHolder {
+
+    @BindView(R.id.item_list)
+    TextView Information;
+    @BindView(R.id.item_list_mail)
+    TextView ListMail;
+    
+    @BindView(R.id.item_list_delete)
+    ImageButton DeleteItem;
+    @BindView(R.id.icone_color)
+    ImageView IconeColor;
+    String SourceInformation;
+
+    public MeetingViewHolder(@NonNull View itemView) {
+        super(itemView);
+        ButterKnife.bind(this, itemView);
+    }
+
+    public void updateWithMeeting(Meeting meeting) {
+        DateFormat dateFormat = new SimpleDateFormat("HH:mm", Locale.FRENCH);
+        IconeColor.setColorFilter(meeting.getColormeeting());
+        SourceInformation = "<b>" + meeting.getLocation() + " - " + dateFormat.format(meeting.getDayTimeCalendar().getTime()) + " - " + meeting.getTopic() + "</b>";
+        this.Information.setText(Html.fromHtml(SourceInformation));
+        this.ListMail.setText(meeting.getList().toString().replace("[", "").replace("]", ""));
+    }
+}
---
+++ P4_05_Code_MARIOT_Mathieu/app/src/main/java/com/example/mareu/ui/FragmentListMeeting.java
@@ -0,0 +1,110 @@
+package com.example.mareu.ui;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.mareu.R;
+import com.example.mareu.Views.ListMeetingAdapter;
+import com.example.mareu.events.DeleteMeetingEvent;
+import com.example.mareu.modele.Meeting;
+import com.example.mareu.service.DummyMeetingApiService;
+import com.example.mareu.service.MeetingApiService;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link FragmentListMeeting#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class FragmentListMeeting extends Fragment {
+
+
+    @BindView(R.id.recycler_view_main)
+    public RecyclerView mRecyclerView;
+    public ListMeetingAdapter mAdapter;
+    public List<Meeting> mMeetingList;
+    private MeetingApiService mApiService = new DummyMeetingApiService();
+
+    public FragmentListMeeting() {
+        // Required empty public constructor
+    }
+
+    // TODO: Rename and change types and number of parameters
+    public static FragmentListMeeting newInstance() {
+        FragmentListMeeting fragment = new FragmentListMeeting();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_list_meeting, container, false);
+        ButterKnife.bind(this, view);
+        configureRecylerView();
+        return view;
+    }
+
+    private void configureRecylerView() {
+        this.mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        mMeetingList = mApiService.getMeetingList();
+        this.mAdapter = new ListMeetingAdapter(this.mMeetingList);
+        this.mRecyclerView.setAdapter(mAdapter);
+    }
+
+    private void initList() {
+        mMeetingList = mApiService.getMeetingList();
+        this.mAdapter = new ListMeetingAdapter(this.mMeetingList);
+        mRecyclerView.setAdapter(mAdapter);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        initList();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        EventBus.getDefault().register(this);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        EventBus.getDefault().unregister(this);
+    }
+
+    @Subscribe
+    public void onDeleteMeeting(DeleteMeetingEvent event) {
+        mApiService.DeleteMeeting(event.meeting);
+        initList();
+        //mAdapter.notifyItemRemoved();
+    }
+
+
+}
